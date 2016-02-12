a52d956bb5aa4f85b6ac33987619da580ca9106c
Rui Peres <rui.peres@mailonline.co.uk>
2016-02-12 11:57:56 +0000

    first commit

---
+++ Vinyl/Vinyl.h
@@ -0,0 +1,19 @@
+//
+//  Vinyl.h
+//  Vinyl
+//
+//  Created by Rui Peres on 12/02/2016.
+//  Copyright © 2016 Velhotes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Vinyl.
+FOUNDATION_EXPORT double VinylVersionNumber;
+
+//! Project version string for Vinyl.
+FOUNDATION_EXPORT const unsigned char VinylVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Vinyl/PublicHeader.h>
+
+
