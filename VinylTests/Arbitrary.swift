164503f0d71728009567ef6baea83653fe9ae645
Rui Peres <rui.peres@mailonline.co.uk>
2016-02-22 11:31:47 +0000

    Added utility builder methods (JSON, Base64 and UTF8)

--- VinylTests/Arbitrary.swift
+++ VinylTests/Arbitrary.swift
@@ -9,36 +9,68 @@
 import SwiftCheck
 
 /// Generates an array of lowercase alphabetic `Character`s.
-let lowerStringGen = 
-    Gen<Character>.fromElementsIn("a"..."z")
+let lowerStringGen =
+Gen<Character>.fromElementsIn("a"..."z")
     .proliferateNonEmpty
     .map(String.init)
 
+let intGen =
+Gen<Int>.fromElementsIn(1...9)
+    .proliferateNonEmpty
+
 /// Generates a URL of the form `(http|https)://<domain>.com`.
-let urlStringGen : Gen<String> = sequence([ 
+let urlStringGen : Gen<String> = sequence([
     Gen<String>.fromElementsOf(["http://", "https://"]),
     lowerStringGen,
     Gen.pure(".com"),
-].reverse()).map { $0.reduce("", combine: +) }
+    ].reverse()).map { $0.reduce("", combine: +) }
+
+// Generates a JSON string of the form '"string"'
+let jsonString: Gen<String> = lowerStringGen.map { "\"" + $0 + "\""}
+
+// Generates a JSON string pair of the form '"key":"value"'
+let jsonStringPair: Gen<String> = sequence([
+    jsonString,
+    Gen.pure(":"),
+    jsonString])
+    .map { $0.reduce("", combine: +) }
+
+// Generates a JSON string pair of the form '"key":"value", "key1":"value1" ....'
+let jsonStringPairs: Gen<String> =  Gen.sized { sz in
+    return jsonStringPair.proliferateSized(sz + 1)
+    } .map { xs in
+        return xs.reduce("") { $0 == "" ? $1 : $0 + "," + $1 }
+}
+
+// Generates a JSON of the form '{"key":"value", "key1":"value1" .... }'
+let basicJSONDic : Gen<AnyObject> = sequence([
+    Gen.pure("{"),
+    jsonStringPairs,
+    Gen.pure("}")
+    ].reverse()
+    )
+    .map { $0.reduce("", combine: +) }
+    .map { $0.dataUsingEncoding(NSUTF8StringEncoding)! }
+    .map { try! NSJSONSerialization.JSONObjectWithData($0, options: .AllowFragments) }
 
 /// Generates a path of the form `<some>/<path>/<to>/.../<somewhere>`.
-let urlPathGen : Gen<String> = 
-    (curry(+) <^> Gen.pure("/") <*> lowerStringGen)
+let urlPathGen : Gen<String> =
+(curry(+) <^> Gen.pure("/") <*> lowerStringGen)
     .proliferate
     .map { $0.reduce("", combine: +) }
 
 /// Generates an array of parameters of the form `<param>=<arg>`,
 let parameterGen : Gen<String> = sequence([
-	lowerStringGen,
-	Gen.pure("="),
-	lowerStringGen,
-].reverse()).map { $0.reduce("", combine: +) }
+    lowerStringGen,
+    Gen.pure("="),
+    lowerStringGen,
+    ].reverse()).map { $0.reduce("", combine: +) }
 
-/// Generates a set of parameters. 
+/// Generates a set of parameters.
 let pathParameterGen : Gen<String> = Gen.sized { sz in
-	return parameterGen.proliferateSized(sz + 1)
-} .map { xs in 
-	return xs.reduce("?") { $0 == "?" ? "?" + $1 : $0 + "&" + $1 } 
+    return parameterGen.proliferateSized(sz + 1)
+    } .map { xs in
+        return xs.reduce("?") { $0 == "?" ? "?" + $1 : $0 + "&" + $1 }
 }
 
 private func curry<A, B, C>(f : (A, B) -> C) -> A -> B -> C {
--- VinylTests/TrackTests.swift
+++ VinylTests/TrackTests.swift
@@ -17,34 +17,77 @@ class TrackTests: XCTestCase {
             return track.response.urlResponse.statusCode == 400
                 && track.response.urlResponse.URL?.absoluteString == url
                 && track.request.URL?.absoluteString == url
-                && track.response.body == nil 
+                && track.response.body == nil
         }
         
         property("Bad tracks created with an error reflect that error") <- forAllNoShrink(urlStringGen) { url in
             return forAll { (domain : String, code : Positive<Int>) in
                 let error = NSError(domain: domain, code: code.getPositive, userInfo: nil)
                 let track = TrackFactory.createBadTrack(NSURL(string: url)!, statusCode: code.getPositive, error: error)
+                
                 return track.response.urlResponse.statusCode == code.getPositive
                     && track.response.error == error
                     && track.response.urlResponse.URL?.absoluteString == url
                     && track.request.URL?.absoluteString == url
-                    && track.response.body == nil 
+                    && track.response.body == nil
             }
         }
         
         property("Well made tracks are well made") <- forAllNoShrink(
-              urlStringGen
+            urlStringGen
             , String.arbitrary
             , HTTPHeaders.arbitrary
-        ) { (url, body, headers) in
-            let data = body.dataUsingEncoding(NSUTF8StringEncoding)!
-            let track = TrackFactory.createValidTrack(NSURL(string: url)!, body: data, headers: headers)
-            
-            return track.response.urlResponse.statusCode == 200
-                && track.response.body!.isEqualToData(data)
-                && track.response.urlResponse.allHeaderFields as! HTTPHeaders == headers
-                && track.response.urlResponse.URL?.absoluteString == url
-                && track.request.URL?.absoluteString == url
+            ) { (url, body, headers) in
+                let data = body.dataUsingEncoding(NSUTF8StringEncoding)!
+                let track = TrackFactory.createValidTrack(NSURL(string: url)!, body: data, headers: headers)
+                
+                return isValidTrack(track, data: data, headers: headers, url: url)
         }
-    }
+        
+        property("Well made tracks for base64") <- forAllNoShrink(
+            urlStringGen
+            , String.arbitrary
+            , HTTPHeaders.arbitrary
+            ) { (url, body, headers) in
+                
+                guard let data = NSData(base64EncodedString: body, options: .IgnoreUnknownCharacters) else { return true }
+                let track = TrackFactory.createValidTrackFromBase64(NSURL(string: url)!, bodyString:body, headers: headers)
+                
+                return isValidTrack(track, data: data, headers: headers, url: url)
+        }
+        
+        property("Well made tracks for utf8") <- forAllNoShrink(
+            urlStringGen
+            , String.arbitrary
+            , HTTPHeaders.arbitrary
+            ) { (url, body, headers) in
+                
+                guard let data = body.dataUsingEncoding(NSUTF8StringEncoding) else { return true}
+                let track = TrackFactory.createValidTrackFromUTF8(NSURL(string: url)!, bodyString:body, headers: headers)
+                
+                return isValidTrack(track, data: data, headers: headers, url: url)
+        }
+        
+        property("Well made tracks for JSON") <- forAllNoShrink(
+            urlStringGen
+            , basicJSONDic
+            , HTTPHeaders.arbitrary
+            ) { (url, body, headers) in
+                
+                print(basicJSONDic)
+                guard let data = try? NSJSONSerialization.dataWithJSONObject(body, options: .PrettyPrinted) else { return true}
+                let track = TrackFactory.createValidTrackFromJSON(NSURL(string: url)!, json:body, headers: headers)
+                
+                return isValidTrack(track, data: data, headers: headers, url: url)
+        }
+    }   
+}
+
+func isValidTrack(track: Track, data: NSData, headers: HTTPHeaders, url: String) -> Bool {
+    
+    return track.response.urlResponse.statusCode == 200
+        && track.response.body!.isEqualToData(data)
+        && track.response.urlResponse.allHeaderFields as! HTTPHeaders == headers
+        && track.response.urlResponse.URL?.absoluteString == url
+        && track.request.URL?.absoluteString == url
 }
