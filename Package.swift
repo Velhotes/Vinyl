ebcec23c80087dc6539b8fb262316b4cfd8e772d
David Rodrigues <david.ndh@gmail.com>
2019-07-24 18:41:11 +0100

    Update package to use the latest SPM tooling and spec (#96)
    * Update package to use the latest SPM tooling and spec
    
    * Update dependencies

--- Package.swift
+++ Package.swift
@@ -1,7 +1,18 @@
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
     name: "Vinyl",
-    exclude: ["Carthage", "VinylTests"]
+    products: [
+        .library(name: "Vinyl", targets: ["Vinyl"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/typelift/SwiftCheck", from: "0.12.0")
+    ],
+    targets: [
+        .target(name: "Vinyl", path: "Vinyl"),
+        .testTarget(name: "VinylTests", dependencies: ["Vinyl", "SwiftCheck"], path: "VinylTests"),
+    ],
+    swiftLanguageVersions: [.v4_2]
 )
 
